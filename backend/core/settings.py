fb31880f99a401c9f81903bf46710b7bf465c218
rb14v1 <ramya.bhaskar@version1.com>
2026-07-13 07:08:51 +0100

    chore: Fix: Configuration externalised

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.x/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i8d*!rjvn$5yj(g_4hxqic7^_564*ptk!&64&0cajh#!m62zk#'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-i8d*!rjvn$5yj(g_4hxqic7^_564*ptk!&64&0cajh#!m62zk#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
 
